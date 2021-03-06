d34a707fbea020848a65f18d4682ecb9818f2b6c
07anshul <ans2852000@gmail.com>
2021-03-06 20:35:14 +0530

    Followers file

---
+++ php/followers.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+if (!isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == false) {
+  header("location: login.php");
+  exit;
+}
+
+// Include config file
+require_once "config.php";
+ ?>
+
+
+<!-- Html - Searched Profile -->
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo $_SESSION["username"]; ?>/Followers</title>
+    <link rel="stylesheet" type="text/css" href="/project/css/searched_profile.css">
+  </head>
+  <body>
+    <?php
+
+    // Prepare join statement
+    $join = "SELECT user_credentials.username FROM user_followers INNER JOIN user_credentials ON user_followers.follower_id = user_credentials.id WHERE user_followers.user_id = ?";
+
+    if ($stmt = $mysqli->prepare($join)) {
+      $stmt->bind_param("i", $param_user_id);
+      $param_user_id = $_SESSION["id"];
+
+      if ($stmt->execute()) {
+        $result = $stmt->get_result();
+        while ($followers = $result->fetch_assoc()) {
+          echo $followers["username"].'<br><br>';
+        }
+      }
+    }
+
+     ?>
+
+  </body>
+</html>

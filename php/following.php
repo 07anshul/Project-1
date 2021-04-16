941871295abdbdae80ab0c35a2da81be6b191dc9
07anshul <ans2852000@gmail.com>
2021-04-16 21:28:53 +0530

    following.php

--- php/following.php
+++ php/following.php
@@ -5,7 +5,7 @@ if (!isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == false) {
   exit;
 }
 // Include config file
-require_once "config.php"
+require_once "config.php";
  ?>
 
 <!-- Html - Searched Profile -->
@@ -14,9 +14,26 @@ require_once "config.php"
   <head>
     <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $_SESSION["username"]; ?>/Following</title>
-    <link rel="stylesheet" type="text/css" href="/project/css/searched_profile.css">
+    <link rel="stylesheet" type="text/css" href="/project/css/following.css">
   </head>
   <body>
+
+    <header>
+      <div class="following_header">
+        <!-- Task bar-->
+        <nav>
+            <div id="following_menu">
+              <ul>
+                <li><a href="/project/index.html">Home</a></li>
+                <li><a href="profile.php">My Profile</a></li>
+                <li><a href="chat_page.php">Lets Chat!</a></li>
+                <li><a href="forum.php">Forum</a></li>
+              </ul>
+            </div>
+        </nav>
+      </div>
+    </header>
+
     <?php
     // Prepare join statement
     $join = "SELECT user_credentials.username FROM user_followers INNER JOIN user_credentials ON user_followers.user_id = user_credentials.id WHERE user_followers.follower_id = ?";
@@ -28,7 +45,8 @@ require_once "config.php"
       if ($stmt->execute()) {
         $result = $stmt->get_result();
         while ($following = $result->fetch_assoc()) {
-          echo $following["username"].'<br><br>';
+
+          echo "<div id=followings> $following[username] </div>";
         }
       }
     }

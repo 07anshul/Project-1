a32b08b61ec9dc522d1a7d48c70a8ada2b9bfe90
07anshul <ans2852000@gmail.com>
2021-04-16 21:29:40 +0530

    post.php

--- php/post.php
+++ php/post.php
@@ -3,14 +3,36 @@ session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-  $current_date_time = 'users_directory/'.$_SESSION["username"].'/'.date("d-m-y").' '.date("h:i:s a");
-  $user_post = fopen("$current_date_time", "w");
-  $text = trim($_POST["post_text"]);
-  fwrite($user_post, $text);
+  $dir_path = 'users_directory/'.$_SESSION["id"].'/';
+  if (!file_exists($dir_path)) {
+      mkdir($dir_path);
+  }
 
+  $current_date_time = $dir_path.date("y-m-d").' | '.date("H:i:s");
+if (!empty($_POST["post_text"])) {
+  if (isset($_POST["insert_post"])) {
+    $user_post = fopen("$current_date_time", "w");
+    $text = trim($_POST["post_text"]);
+    fwrite($user_post, $text);
+    header("location: profile.php");
+  }
+}
+else {
+  header("location: profile.php");
 }
 
+  if (isset($_POST["delete_post"])) {
 
+    $post_address = $_POST["post_address"];
+
+    echo "<script>alert('Do You Really Want To Delete The Post PERMANENTLY!')</script>";
+
+    unlink("$dir_path$post_address");
+    header("location: profile.php");
+
+  }
+fclose($user_post);
+}
 
 
 ?>
